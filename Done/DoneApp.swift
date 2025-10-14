e1ab66baf57344e70771ab625876ac49211a867f
szymons12 <szymon.sobelski@gmail.com>
2025-10-14 19:18:43 +0200

    Initial Commit

---
+++ Done/DoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoneApp.swift
+//  Done
+//
+//  Created by Szymon on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DoneTests/DoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  DoneTests.swift
+//  DoneTests
+//
+//  Created by Szymon on 14/10/2025.
+//
+
+import Testing
+@testable import Done
+
+struct DoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
